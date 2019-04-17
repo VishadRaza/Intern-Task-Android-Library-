529d5f965aeeaa98a9bea20e507261f0fa336467
Vishad <syedavishadrazazaidi@gmail.com>
2019-04-17 13:05:46 +0500

    Add a new method

--- toasterlibrary/src/main/java/com/foodbreak/vishad/toasterlibrary/ToasterMessage.java
+++ toasterlibrary/src/main/java/com/foodbreak/vishad/toasterlibrary/ToasterMessage.java
@@ -1,12 +1,19 @@
 package com.foodbreak.vishad.toasterlibrary;
 
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
 
 public class ToasterMessage {
+
     public static void sendmessage(Context c, String message){
 
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
 
+    }
+
+    public static Intent testTask(Context context, Class className){
+        Intent intent = new Intent(context, className);
+        return intent;
     }
 }
