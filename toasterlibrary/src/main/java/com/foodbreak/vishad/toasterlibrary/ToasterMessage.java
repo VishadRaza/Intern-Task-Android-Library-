2e2e9ee08b41083bb91c05d37d9adda28ea83295
Vishad <syedavishadrazazaidi@gmail.com>
2019-04-17 21:28:01 +0500

    Back

--- toasterlibrary/src/main/java/com/foodbreak/vishad/toasterlibrary/ToasterMessage.java
+++ toasterlibrary/src/main/java/com/foodbreak/vishad/toasterlibrary/ToasterMessage.java
@@ -1,11 +1,13 @@
 package com.foodbreak.vishad.toasterlibrary;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class ToasterMessage {
+   public static GoogleActivity n = new GoogleActivity();
 
     public static void sendmessage(Context c, String message){
 
@@ -17,7 +19,7 @@ public class ToasterMessage {
 
         Intent intent = new Intent(context, GoogleActivity.class);
         //intent.setClass(context,className);
-          
+      n.onBackPressed();
 
         return intent;
     }
